f17ace705a5fbe1701c0b4ce918c9276f761cdeb
Remes Anssi TTK17SP <anssiremes@kamk.fi>
2017-12-02 15:54:34 +0200

    v1.0

---
+++ President Evil/Assets/Scripts/CharacterSelectScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelectScript : MonoBehaviour
+{
+    public string player1Selection = "", player2Selection = "";
+
+    public float secondsBeforePlaying;
+
+    public int CombatSceneIndex;
+
+    bool charactersSelected = false;
+
+    void Update()
+    {
+
+        if(player1Selection != "" && player2Selection != "")
+        {
+            charactersSelected = true;
+        }
+
+        if (charactersSelected)
+        {
+            secondsBeforePlaying -= Time.deltaTime;
+            if(secondsBeforePlaying <= 0)
+            {
+                SceneManager.LoadScene(CombatSceneIndex);
+            }
+        }
+    }
+
+    public void SelectKekkonen()
+    {
+        if(player1Selection == "")
+        {
+            player1Selection = "Kekkonen";
+            GameObject.Find("/HahmovalikkoCanvas/Player1Select/Kekkonen").SetActive(true);
+        }
+        else if(player1Selection != "" && player2Selection == "")
+        {
+            player2Selection = "Kekkonen";
+            GameObject.Find("/HahmovalikkoCanvas/Player2Select/Kekkonen").SetActive(true);
+        }
+    }
+    public void SelectSale()
+    {
+        if (player1Selection == "")
+        {
+            player1Selection = "Sauli";
+            GameObject.Find("/HahmovalikkoCanvas/Player1Select/Sauli").SetActive(true);
+        }
+        else if (player1Selection != "" && player2Selection == "")
+        {
+            player2Selection = "Sauli";
+            GameObject.Find("/HahmovalikkoCanvas/Player2Select/Sauli").SetActive(true);
+        }
+    }
+    public void SelectMarski()
+    {
+        if (player1Selection == "")
+        {
+            player1Selection = "Marski";
+            GameObject.Find("/HahmovalikkoCanvas/Player1Select/Marski").SetActive(true);
+        }
+        else if (player1Selection != "" && player2Selection == "")
+        {
+            player2Selection = "Marski";
+            GameObject.Find("/HahmovalikkoCanvas/Player2Select/Marski").SetActive(true);
+        }
+    }
+    public void SelectPukki()
+    {
+        if (player1Selection == "")
+        {
+            player1Selection = "Pukki";
+            GameObject.Find("/HahmovalikkoCanvas/Player1Select/Pukki").SetActive(true);
+        }
+        else if (player1Selection != "" && player2Selection == "")
+        {
+            player2Selection = "Pukki";
+            GameObject.Find("/HahmovalikkoCanvas/Player2Select/Pukki").SetActive(true);
+        }
+    }
+}
---
+++ President Evil/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDScript : MonoBehaviour
+{
+    public GameObject returnButton;
+    public Slider healthBar1, healthBar2;
+    public Text timerText, winnerText;
+
+    public int startTime, startHealths;
+
+    float time;
+
+    bool player1Wins = false;
+    bool player2Wins = false;
+    bool draw = false;
+
+    void Start()
+    {
+        healthBar1.value = startHealths;
+        healthBar2.value = startHealths;
+
+        time = startTime;
+        timerText.text = startTime.ToString();
+    }
+
+    void Update()
+    {
+        RoundTimer();
+        CheckHealth();
+        CheckFlags();
+    }
+    void RoundTimer()
+    {
+        //ROUND TIMER
+        //COUNTDOWN
+        time -= Time.deltaTime;
+        if (time < 10)
+        {
+            timerText.text = ("0" + (time).ToString());
+        }
+        else
+        {
+            timerText.text = (time).ToString();
+        }
+        //ROUND END
+        if (time <= 0)
+        {
+            timerText.text = "00";
+            if (healthBar1.value > healthBar2.value)
+            {
+                player1Wins = true;
+            }
+            else if (healthBar1.value < healthBar2.value)
+            {
+                player2Wins = true;
+            }
+            else
+            {
+                draw = true;
+            }
+        }
+    }
+    void CheckHealth()
+    {
+        if (healthBar1.value <= 0)
+        {
+            player2Wins = true;
+        }
+        else if (healthBar2.value <= 0)
+        {
+            player1Wins = true;
+        }
+        else if (healthBar1.value <= 0 && healthBar2.value <= 0)
+        {
+            draw = true;
+        }
+    }
+    void CheckFlags()
+    {
+        if (player1Wins)
+        {
+            winnerText.text = "PLAYER 1 WINS!";
+            returnButton.SetActive(true);
+        }
+        else if (player2Wins)
+        {
+            winnerText.text = "PLAYER 2 WINS!";
+            returnButton.SetActive(true);
+        }
+        else if (draw)
+        {
+            winnerText.text = "ROUND IS DRAW!";
+            returnButton.SetActive(true);
+        }
+    }
+}
